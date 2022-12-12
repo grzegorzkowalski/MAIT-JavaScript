1789c12c4dc86a1a9a1205dd895bb7115a0b6372
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2022-12-12 18:07:45 +0100

    Chapter1 Task2

--- 01_Wprowadzenie_do_JS/Zadanie_02/main.js
+++ 01_Wprowadzenie_do_JS/Zadanie_02/main.js
@@ -2,20 +2,27 @@ if (true) {
 	let a = "A";
 	const b = "B";
 	var c = "C";
-} 
+	console.log(a);
+}
 
-let one = 1; 
-const two = 2; 
+let one = 1;
+const two = 2;
 var three = 3;
-
+console.log(one);
+console.log(two);
+console.log(three);
 function testVars() {
 	let dog = "pekińczyk";
 	const cat = "syjamski";
 	var chomik = "syryjski";
+	console.log(chomik);
 }
+testVars();
+console.log(chomik);
+console.log(c);
 
 
 /*
 Wnioski:
-
+Nie  ma dostępu do zmiennych let i const w bloku.
 */
