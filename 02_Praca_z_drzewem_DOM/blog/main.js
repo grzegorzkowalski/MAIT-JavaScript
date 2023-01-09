1afa38b86b9e95bdad551126cdde719f743d3585
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-01-09 17:55:06 +0100

    dom object model task 1

---
+++ 02_Praca_z_drzewem_DOM/blog/main.js
@@ -0,0 +1,8 @@
+const hed = document.querySelector(".about-blog__content-title");
+console.log(hed);
+
+const navigatorLogo = document.querySelector(".navigation-menu img");
+console.log(navigatorLogo);
+
+const form = document.querySelector("form");
+console.log(form);
