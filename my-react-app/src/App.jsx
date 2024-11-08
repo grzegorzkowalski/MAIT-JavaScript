2a12e91291448d268083a9d4cfa5df935a4d65a1
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-11-08 19:26:10 +0100

    add attributes task 4

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,30 @@
 import './App.css'
 
 function App() {
-  const num1 = parseInt(prompt("Podaj pierwszą liczbę?"));
-  const num2 = parseInt(prompt("Podaj drugą liczbę?"));
+  //const num1 = parseInt(prompt("Podaj pierwszą liczbę?"));
+  //const num2 = parseInt(prompt("Podaj drugą liczbę?"));
+  const imageUrl = "https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_960_720.jpg";
+  const color = prompt("Wybierz kolor (red, green, blue)?");
+  const colors = ["red", "green", "blue"];
+  let borderColor = "";
+
+  if (colors.includes(color)) {
+    borderColor = color;
+  }
+
+  const styles = {
+    "width": "100px",
+    "height": "100px",
+    "borderWidth": "5px",
+    "borderStyle": "solid",
+    color
+  };
+
   return (
     <>
-        <p>{num1 + num2}</p>
+      {/*<p>{num1 + num2}</p>*/}
+      {/*<img src={imageUrl} alt="car" />*/}
+      {borderColor ? <div style={styles} /> : <div>Niepoprawny kolor</div>}
     </>
   )
 }
