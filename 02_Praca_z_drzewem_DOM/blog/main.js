5a5d88d62fae94aa9667becdbcc56fd52506b88a
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-11-10 11:22:13 +0100

    start work with dom tree

---
+++ 02_Praca_z_drzewem_DOM/blog/main.js
@@ -0,0 +1,17 @@
+//Zadanie 1
+
+// pkt. 1
+const header = document.querySelector(".about-blog__content-title");
+console.log(header);
+
+// pkt. 2
+const logo = document.querySelector(".navigation-menu > img");
+console.log(logo);
+
+// pkt. 3
+const blogForm = document.querySelector("form");
+console.log(blogForm);
+
+// pkt. 4
+const link = document.querySelector('a[href="mailto:grzegorz@theadventurerblog.pl"]');
+console.log(link);
