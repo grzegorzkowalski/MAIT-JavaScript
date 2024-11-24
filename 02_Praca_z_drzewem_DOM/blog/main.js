d3c5efb8c3d642a19e63ee0022bb21f5cd01b11d
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-11-24 10:52:29 +0100

    task 3 chapter 2

--- 02_Praca_z_drzewem_DOM/blog/main.js
+++ 02_Praca_z_drzewem_DOM/blog/main.js
@@ -20,3 +20,42 @@ console.log(link);
 
 const galleryItems = Array.from(document.querySelectorAll(".gallery-main-photo"));
 console.log(galleryItems);
+
+//Zadanie 3
+
+// pkt. 1
+console.log(header.textContent);
+console.log(header.innerText);
+
+header.textContent = "    Interpretuje białe znaki   ";
+
+// pkt. 2
+console.log(logo.tagName);
+
+// pkt. 3
+const warsawLogo = "https://nowymarketing.pl/wp-content/uploads/2022/12/40680_l2-360x0-c-default.jpg";
+logo.setAttribute("src", warsawLogo);
+
+// pkt. 4
+console.log(blogForm.innerHTML);
+
+// pkt. 5
+link.setAttribute("href", "kontakt@wwsi.edu.pl");
+
+// pkt. 6
+const headerClassName = header.className;
+const headerClassList = header.classList.value;
+console.log(headerClassName);
+console.log(headerClassList);
+
+// pkt. 7
+// header.className = "";
+// console.log(header.className);
+
+header.classList.remove(headerClassList);
+console.log(header.classList.value);
+console.log(headerClassList);
+
+// pkt. 8
+//document.querySelector(".first-article__text-title").classList.add(headerClassList);
+document.querySelector(".first-article__text-title").className += " " + headerClassList;
